ca8806a363e1076adbf315e13f535f69452e03b9
nate.nystrom <nate.nystrom@a0d92d08-0619-11df-8138-214a9d281a8c>
2010-02-08 22:43:20 +0000

    Minor cleanups.  Added a new test case.

--- funicular.test/src/funicular/test/MontyPi.scala
+++ funicular.test/src/funicular/test/MontyPi.scala
@@ -3,15 +3,15 @@ package funicular.test
 import funicular.Intrinsics._
 import funicular.X10Application
 import funicular.Config
-import scala.util.Random;
+import scala.util.Random
 
 object MontyPi extends X10Application {
     def run(s: Array[String]) {
         println("begin")
 
         if (s.length != 1) {
-            println("Usage: MontyPi <number of points>");
-            exit(-1);
+            println("Usage: MontyPi <number of points>")
+            exit(-1)
         }
 
         val P = Config.NTHREADS
@@ -20,20 +20,20 @@ object MontyPi extends X10Application {
 
         val initializer = (i:Int) => {
             val r = new Random
-            var result: Double = 0.0D;
+            var result = 0.0
             for (j <- 1 to N) {
                 val x = r.nextDouble
                 val y = r.nextDouble
-                if (x*x +y*y <= 1.0)
-                    result += 1
+                if (x*x + y*y <= 1.0)
+                    result += 1.0
             }
             result
-        };
+        }
 
         val result = new Array[Double](P).parInit(initializer)
 
         val pi = 4 * result.reduce((x:Double,y:Double) => x+y, 0) / (N*P)
 
-        println("The value of pi is " + pi);
+        println("The value of pi is " + pi)
     }
 }
