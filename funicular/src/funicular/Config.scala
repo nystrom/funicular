4d6d7211af498e40e9ed6075468bdcb33152f7f1
nate.nystrom <nate.nystrom@a0d92d08-0619-11df-8138-214a9d281a8c>
2010-03-07 04:15:04 +0000

    Make NTHREADS by lazily initialized.

--- funicular/src/funicular/Config.scala
+++ funicular/src/funicular/Config.scala
@@ -1,7 +1,7 @@
 package funicular
 
 object Config {
-    val NTHREADS = java.lang.Runtime.getRuntime.availableProcessors
+    lazy val NTHREADS = java.lang.Runtime.getRuntime.availableProcessors
     val BOUND = 100
     val MAX_WORKERS = 1000
     val MAXTHREADS = 32767
