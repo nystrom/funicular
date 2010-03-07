66e84f7cb736ef478cf073e31e9f270b2590dfcc
nate.nystrom <nate.nystrom@a0d92d08-0619-11df-8138-214a9d281a8c>
2010-03-07 17:56:23 +0000

    Add some pattern matching for MultipleExceptions

--- trunk/funicular/src/funicular/MultipleExceptions.scala
+++ trunk/funicular/src/funicular/MultipleExceptions.scala
@@ -1,10 +1,19 @@
 package funicular
 
-class MultipleExceptions(exceptions: Collection[Throwable]) extends Exception {
+class MultipleExceptions(val exceptions: Collection[Throwable]) extends Exception {
     override def printStackTrace = {
         super.printStackTrace
         println("Caused by:")
         for (th <- exceptions)
            th.printStackTrace
+    }
+}
+
+object MultipleExceptions {
+    def unapply(v: Any) = {
+        if (v.isInstanceOf[MultipleExceptions])
+            Some(v.asInstanceOf[MultipleExceptions].exceptions.toList)
+        else
+            None
     }
 }
