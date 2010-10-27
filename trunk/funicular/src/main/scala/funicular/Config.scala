b3c41ae206f63cec811ce29fa1246e35436d7c56
nate.nystrom <nate.nystrom@a0d92d08-0619-11df-8138-214a9d281a8c>
2010-10-27 20:02:06 +0000

    Change NTHREADS to avail processors.

--- trunk/funicular/src/main/scala/funicular/Config.scala
+++ trunk/funicular/src/main/scala/funicular/Config.scala
@@ -1,8 +1,8 @@
 package funicular
 
 object Config {
-    lazy val NTHREADS = (java.lang.Runtime.getRuntime.availableProcessors * 3.5).toInt
-    // lazy val NTHREADS = java.lang.Runtime.getRuntime.availableProcessors
+    // lazy val NTHREADS = (java.lang.Runtime.getRuntime.availableProcessors * 3.5).toInt
+    lazy val NTHREADS = java.lang.Runtime.getRuntime.availableProcessors
     val BOUND = 100
     val MAX_WORKERS = 1000
     val MAXTHREADS = 32767
